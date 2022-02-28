ded75174907064d185d50e24c54739e91402eaa0
Daniel Octavio Ramírez Neri <l21111240@cdjuarez.tecnm.mx>
2022-02-28 14:11:50 -0700

    Esto es una prueba

---
+++ src/main/java/Paquetes/Prueba.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquetes;
+
+/**
+ *
+ * @author esemi
+ */
+public class Prueba {
+    private String nombre;
+
+    public Prueba(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Prueba{" + "nombre=" + nombre + '}';
+    }
+    
+    
+}
